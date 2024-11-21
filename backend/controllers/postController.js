70149b93f7b020f43f87944d60b8fc1b42668bed
MayankSharma <69496455+mayankongithub@users.noreply.github.com>
2024-11-22 01:37:51 +0530

    post

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -5,7 +5,7 @@ import Post from '../models/postModel.js'
 import { message } from "../utils/message.js";
 export const createPost = async (req, res) => {
     try{
-        const {caption, location, mentions, comments,image} = req.body;
+        const {caption, location,image} = req.body;
         const user_present = await User.findById(req.user._id);
         if(!user_present){
             res.status(401).json({
@@ -36,9 +36,9 @@ export const createPost = async (req, res) => {
         })
             
         
-        await newPost.save();
-        if (newuser) {
-            User.posts.unshift(newPost._id);
+        await post.save();
+        if(post){
+            User.posts.unshift(post._id);
             await User.save();
         }
         
@@ -54,3 +54,63 @@ export const createPost = async (req, res) => {
     }
 }
 
+//get all post
+export const getAllPosts = async (req, res) => {
+    try {
+        const posts = await Post.find();
+        res.status(200).json({
+            success : true,
+            message : "Success",
+            posts
+        })
+    } 
+    catch (error) {
+        res.status(500).json({ message: "No Post", error: error.message });
+    }
+};
+
+
+//get post by id
+export const getPostsById = async (req, res) => {
+    try {
+        const {id} = req.params;
+        const posts = await Post.findById(id);
+        if(!posts){
+            return res.status(404).json({
+                success: false,
+                message: "Post not found",
+            });
+        }
+        res.status(200).json({
+            success: true,
+            message: "Post found successfully",
+            post: posts
+        }); 
+        
+    } 
+    catch (error) {
+        res.status(500).json({ message: "No post", error: error.message });
+    }
+};
+
+
+export const deletePostById = async(req,res) => {
+    try {
+        const postId = req.params.id;
+        if(!postId){
+            return res.status(404).json({
+                success: false,
+                message: "User not found",
+            });
+        }
+        await Post.findByIdAndDelete(postId);
+
+        res.status(200).json({ 
+            success: true, 
+            message: "Post deleted successfully" 
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ success: false, message: "Server Error" });
+    }
+}
--- backend/routes/postRoute.js
+++ backend/routes/postRoute.js
@@ -1,8 +1,11 @@
 import express from "express";
-import { createPost } from "../controllers/postController.js";
+import { createPost,deletePostById,getAllPosts, getPostsById} from "../controllers/postController.js";
 import {isAuthenticated} from '../middleware/auth.js'
 const postRouter = express.Router();
 
-postRouter.post("/create", isAuthenticated,createPost);
+postRouter.post("/create/post", isAuthenticated,createPost);
+postRouter.get("/getAllPosts", isAuthenticated,getAllPosts);
+postRouter.get("/getPost/:id", isAuthenticated,getPostsById);
+postRouter.delete("/deletePostBy/:id", isAuthenticated,deletePostById);
 
 export default postRouter;
